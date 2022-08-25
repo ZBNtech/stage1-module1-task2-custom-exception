341d3014a3e1ac37b8b21691500b540d7b8e0417
schedulerz <thisisabilkair.zhakupov@gmail.com>
2022-08-25 23:07:48 +0600

    #2

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -19,8 +19,16 @@ public class StudentManager {
     StudentManager manager = new StudentManager();
 
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+
+      try {
+
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
+
+      } catch (StudentNotFoundException ex) {
+        System.err.print(ex);
+      }
+
     }
 
   }
